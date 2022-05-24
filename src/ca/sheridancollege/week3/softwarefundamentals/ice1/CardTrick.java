f7ff58cfff760ba225498c7c8631f80447baefcd
Jorawar <sin11529@10.0.0.236>
2022-05-24 18:00:54 -0400

    Added name and student id

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -9,24 +9,40 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author ----
+ * @author Jorawar - 991645645
  */
+import java.util.*;
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        Scanner sc = new Scanner(System.in);
         for (int i=0; i<magicHand.length; i++)
         {
+            Random rn = new Random();
+            int number = rn.nextInt(13) + 1;
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(number);
+            int newNumber = rn.nextInt(4);
+            c.setSuit(Card.SUITS[newNumber]);
+            magicHand[i] = c;
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        System.out.print("Enter the card suit: ");
+        String cardSuit = sc.nextLine();
+        System.out.print("Enter the card number: ");
+        int cardNumber = sc.nextInt();
+        boolean cardFound = false;
+        for(int i = 0; i < magicHand.length; i++) {
+            if(magicHand[i].getValue() == cardNumber && magicHand[i].getSuit().equals(cardSuit)) {
+                System.out.println("Card found!");
+                System.out.println("Card Suit: "+ magicHand[i].getSuit());
+                System.out.println("Card Value: "+ magicHand[i].getValue());
+                cardFound = true;
+            }
+        }
+        if(!cardFound) {
+            System.out.println("Card not found!");
+        }
     }
-    
 }
